57e7bf71d237b5be8803c47275291c05e768e308
Gerardo <gerardoesposito38@gmail.com>
2023-08-30 18:25:49 -0300

    getters y setters

--- src/main/java/com/example/demo/model/Estudiante.java
+++ src/main/java/com/example/demo/model/Estudiante.java
@@ -6,8 +6,32 @@ public class Estudiante {
     private Integer legajo;
 
     public Estudiante(long idEstudiante, long idPersona, Integer legajo){
-        this idEstudiante=idEstudiante;
-        this idPersona=idPersona;
-        this legajo=legajo;
+        this.idEstudiante=idEstudiante;
+        this.idPersona=idPersona;
+        this.legajo=legajo;
+    }
+    public long getIdEstudiante() {
+        return idEstudiante;
+    }
+    public void setIdEstudiante(long idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public long getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(long idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public Integer getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(Integer legajo) {
+        this.legajo = legajo;
     }
 }
+
+
