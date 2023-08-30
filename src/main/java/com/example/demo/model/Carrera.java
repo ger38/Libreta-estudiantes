03925f018f9f8df9ec3b5574cf556650f4708e09
Gerardo <gerardoesposito38@gmail.com>
2023-08-30 18:33:39 -0300

    Getters y setters

--- src/main/java/com/example/demo/model/Carrera.java
+++ src/main/java/com/example/demo/model/Carrera.java
@@ -5,7 +5,23 @@ public class Carrera {
     private String nombre;
 
     public Carrera(long idCarrera, String nombre){
-        this idCarrera=idCarrera;
-        this nombre=nombre;
+        this.idCarrera=idCarrera;
+        this.nombre=nombre;
+    }
+    public long getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(long idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
+
