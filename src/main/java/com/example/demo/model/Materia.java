17044e12a12b2a643465313a6f5233502af554b7
Gerardo <gerardoesposito38@gmail.com>
2023-08-30 18:41:44 -0300

    Getters y setters

--- src/main/java/com/example/demo/model/Materia.java
+++ src/main/java/com/example/demo/model/Materia.java
@@ -7,10 +7,51 @@ public class Materia {
     private long idProfesor;
 
     public Materia(long idMateria, String nombre, Integer anio, long idCarrera, long idProfesor){
-        this idMateria=idMateria;
-        this nombre=nombre;
-        this anio=anio;
-        this idCarrera=idCarrera;
-        this idProfesor=idProfesor;
+        this.idMateria=idMateria;
+        this.nombre=nombre;
+        this.anio=anio;
+        this.idCarrera=idCarrera;
+        this.idProfesor=idProfesor;
+    }
+    public long getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(long idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Integer anio) {
+        this.anio = anio;
+    }
+
+    public long getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(long idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public long getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(long idProfesor) {
+        this.idProfesor = idProfesor;
     }
 }
+
+
