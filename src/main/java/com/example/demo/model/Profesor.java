a474133f1b76898fa2aec19d0eca0b452e1ad12b
Gerardo <gerardoesposito38@gmail.com>
2023-08-30 18:58:58 -0300

    Complete la clase profesor

--- src/main/java/com/example/demo/model/Profesor.java
+++ src/main/java/com/example/demo/model/Profesor.java
@@ -1,2 +1,28 @@
-package com.example.demo.model;public class Profesor {
+package com.example.demo.model;
+public class Profesor {
+    private long idProfesor;
+    private long idPersona;
+
+    public  Profesor(long idProfesor, long idPersona){
+        this.idProfesor=idProfesor;
+        this.idPersona=idPersona;
+    }
+
+    public long getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(long idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public long getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(long idPersona) {
+        this.idPersona = idPersona;
+    }
 }
+
+
